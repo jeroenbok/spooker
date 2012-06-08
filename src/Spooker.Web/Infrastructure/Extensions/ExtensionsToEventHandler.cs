8f594599b6487f77a00c27eb1fc1c0d3d9fa1c6d
robvdlv <robvdlv@gmail.com>
2012-06-08 22:33:08 +0200

    Add infrastructure for ie. simple raising of events.

---
+++ src/Spooker.Web/Infrastructure/Extensions/ExtensionsToEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Spooker.Web.Infrastructure.Extensions
+{
+    public static class ExtensionsToEventHandler
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Raise(this EventHandler eventHandler, object sender, EventArgs e)
+        {
+            if (eventHandler == null)
+                return;
+            eventHandler(sender, e);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Raise<T>(this EventHandler<T> eventHandler, object sender, T e) where T : EventArgs
+        {
+            if (eventHandler == null)
+                return;
+            eventHandler(sender, e);
+        }
+    }
+}
