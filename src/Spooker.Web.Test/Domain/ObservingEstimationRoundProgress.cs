aba19d9cc99632502003f69d8049ada270299b3e
Jeroen Bok <jeroenbok@gmail.com>
2012-06-13 21:12:40 +0200

    Scenario [Seeing estimation round progress] EstimationStatus now reports estimates and participants count

--- src/Spooker.Web.Test/Domain/ObservingEstimationRoundProgress.cs
+++ src/Spooker.Web.Test/Domain/ObservingEstimationRoundProgress.cs
@@ -38,6 +38,18 @@ namespace Spooker.Web.Test.Domain
             Assert.That(round.Status, Is.EquivalentTo(new[] { new Estimate("estimator", StoryPoints.Three), NoEstimate("nonEstimator") }), "estimates");
         }
 
+        [Test]
+        public void When_one_participant_has_estimated_and_another_has_not_then_round_status_has_1_estimate_and_2_participants()
+        {
+            var round = new EstimationRound();
+            var estimator = Participant.In(round, "estimator");
+            Participant.In(round, "nonEstimator");
+            estimator.Estimate(StoryPoints.Three);
+
+            Assert.That(round.Status.EstimateCount, Is.EqualTo(1), "number of estimates");
+            Assert.That(round.Status.ParticipantCount, Is.EqualTo(2), "number of participants");
+        }
+
         private Estimate NoEstimate(string participantName)
         {
             return new Estimate(participantName, StoryPoints.None);
--- src/Spooker.Web/Domain/EstimationStatus.cs
+++ src/Spooker.Web/Domain/EstimationStatus.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Spooker.Web.Domain
 {
     public class EstimationStatus : IEnumerable<Estimate>
     {
-        private readonly IList<Estimate> _estimations;
+        private readonly IList<Estimate> _estimationsOfAllParticipants;
 
-        public EstimationStatus(IList<Estimate> estimations)
+        public EstimationStatus(IList<Estimate> estimationsOfAllParticipants)
         {
-            _estimations = estimations;
+            _estimationsOfAllParticipants = estimationsOfAllParticipants;
         }
 
         public IEnumerator<Estimate> GetEnumerator()
         {
-            return _estimations.GetEnumerator();
+            return _estimationsOfAllParticipants.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public int EstimateCount
+        {
+            get { return _estimationsOfAllParticipants.Count(e => e.StoryPoints != StoryPoints.None);  }
+        }
+
+        public int ParticipantCount
+        {
+            get { return _estimationsOfAllParticipants.Count; }
         }
     }
 }
